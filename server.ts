ed0d11b024927eb601855120d4df58c484322b6a
SaidHoja <saidhojabusiness@gmail.com>
2023-12-02 18:27:51 -0500

    changed paths

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import express, {Request , Response} from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
@@ -15,7 +15,7 @@ app.get('/', (req: Request, res:Response) =>
 })
 
 
-app.get('/login/', (req: Request, res: Response) => {
+app.get('/login/:user', (req: Request, res: Response) => {
   connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
@@ -24,17 +24,17 @@ app.get('/login/', (req: Request, res: Response) => {
    
     console.log('connected as id ' + connection.threadId);
   });
-
-  connection.query({
-    sql: 'SELECT * FROM ‘user’',
-    timeout: 40000, // 40s
-  }, function (error, results, fields) {
+  let useremail = req.params.user;
+  console.log("before query")
+  connection.query(
+    'SELECT * FROM ‘user’ where email = ?',[useremail]
+  , function (error, results, fields) {
     // error will be an Error if one occurred during the query
     // results will contain the results of the query
     // fields will contain information about the returned results fields (if any)
     res.json(results)
   });
-  connection.end(); 
+  console.log("before end")
 })
 
 
